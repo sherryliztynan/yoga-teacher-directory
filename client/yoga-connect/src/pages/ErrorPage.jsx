d4b8eeb7225c25fba6dbb0c970fe670f00289020
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-12-27 10:57:12 -0500

    set up addTeacher route and skeleton pages

---
+++ client/yoga-connect/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Error from '../../components/error/error'
+import Title from '../components/title/title'
+import './errorpage.css'
+
+export default () => (
+  <div className="error">
+    <Title />
+    <Error/>
+  </div>
+)
